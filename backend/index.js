664decd4b883da74047f5030181728ffc93f9c4d
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-26 17:04:09 +0100

    Add post mapping to index.js. Use middleware from express for parsing request and response as jsons. 13:12

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,47 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send("Welcome to MERN. We be merning so hard rn.")
 });
 
+// Route for saving a new book
+app.post("/books", async (request, response) => {
+    try {
+        // Check if request body is of correct type
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+
+        // Create new book object
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        
+        // Call book model which lets mongoose create and add a new entry to mongodb db.
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
