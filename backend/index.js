c14b9e89de6eb097209613685896306b46742637
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-26 17:20:11 +0100

    Add route to get a book by its id to index.js. 16:31

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,18 @@ app.get("/books", async (request, response) => {
     }
 })
 
+// Route to get all books from database
+app.get("/books/:id", async (request, response) => {
+    try {
+        const {id} = request.params
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
