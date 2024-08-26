673dc5cf1e13bc7dbf40c2a7fab418e4958ff421
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-26 15:23:53 +0100

    Create first http request in index.js. 6:00

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js"
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send("Welcome to MERN. We be merning so hard rn.")
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 })
