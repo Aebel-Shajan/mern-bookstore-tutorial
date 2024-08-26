6f994281b9ffcc7045e8b3bf9bfa7e87acf98556
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-26 17:45:44 +0100

    Add route to update a book by it id.  18:30

--- backend/index.js
+++ backend/index.js
@@ -69,6 +69,33 @@ app.get("/books/:id", async (request, response) => {
     }
 })
 
+// Route to update a book
+app.put("/books/:id", async (request, response) => {
+    try {
+        // Check if request body is of correct type
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+        
+        const {id} = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({message: "Book not found"});
+        }
+        return response.status(200).send({message: 'Book updated successfully'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
