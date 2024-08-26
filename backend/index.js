83fe73e10e9ecac6271ae9ca598c644abc71f0d2
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-26 17:12:00 +0100

    Add get request for all books in index.js. 15:00

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,20 @@ app.post("/books", async (request, response) => {
     }
 })
 
+// Route to get all books from database
+app.get("/books", async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
